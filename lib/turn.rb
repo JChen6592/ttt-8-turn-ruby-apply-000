cb16375032e3ab684c5b3fbf7a6fddfda635aa98
John Chen <johnchen062094@gmail.com>
2020-04-26 00:19:44 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,41 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  player_move = board 
+  index = input_to_index(player_move)
+  while 
+end
+
+def input_to_index(move)
+  index = move.to_i - 1
+  return index 
+end 
+
+def position_taken?(board, move)
+  if board[move] == " " || board[move] == "" || board[move] == nil 
+    return false 
+  else 
+    return true 
+  end
+end
+
+def valid_move?(board, move)
+  if move > board.length || 1 > move 
+    return false 
+  elsif position_taken?(board, move)
+    return false 
+  else 
+    return true 
+  end
+end
+    
+def move(board, index, player_char = "X")
+  board[index] = player_char 
+end 
